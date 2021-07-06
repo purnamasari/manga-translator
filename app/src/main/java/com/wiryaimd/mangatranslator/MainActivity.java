417eb90e9d5fc633590546eb2ea84caa1be692b1
wiryaimd <adnyasutha003@gmail.com>
2021-07-06 21:03:11 +0800

    test computer vision

--- app/src/main/java/com/wiryaimd/mangatranslator/MainActivity.java
+++ app/src/main/java/com/wiryaimd/mangatranslator/MainActivity.java
@@ -1,16 +1,102 @@
 package com.wiryaimd.mangatranslator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.TextRecognizerOptions;
+import com.google.mlkit.vision.text.TextRecognizerOptionsInterface;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
+
+    // TODO ================================ VV BACA CUKKZ
+    // NOTE: harus punya google play services
+    // biar bisa download module cv/textrecognition nya, biar ga crash
+    // TODO =================================
+
+
+    private static final String TAG = "MainActivity";
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.main_btn);
 
+        Log.d(TAG, "onCreate: start process");
+
+        TextRecognizer textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.testimg3);
+                InputImage inputImage = InputImage.fromBitmap(bitmap, 0);
+
+                Task<Text> task = textRecognizer.process(inputImage).addOnCompleteListener(new OnCompleteListener<Text>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<Text> task) {
+                        String res = task.getResult().getText();
+                        Log.d(TAG, "onComplete: boom bitch: " + res);
+
+                        for (Text.TextBlock block : task.getResult().getTextBlocks()) {
+                            String blockText = block.getText();
+                            Point[] blockCornerPoints = block.getCornerPoints();
+                            Rect blockFrame = block.getBoundingBox();
+                            Log.d(TAG, "onComplete: blockText: " + blockText);
+                            if (blockFrame != null) {
+                                Log.d(TAG, "onComplete: top: " + blockFrame.top);
+                                Log.d(TAG, "onComplete: bottom: " + blockFrame.bottom);
+                                Log.d(TAG, "onComplete: left: " + blockFrame.left);
+                                Log.d(TAG, "onComplete: right: " + blockFrame.right);
+                                Log.d(TAG, "onComplete: \n");
+                            }
+                            for (Text.Line line : block.getLines()) {
+                                String lineText = line.getText();
+                                Log.d(TAG, "onComplete: lineText: " + lineText);
+                                Log.d(TAG, "onComplete: \n");
+                                Point[] lineCornerPoints = line.getCornerPoints();
+                                Rect lineFrame = line.getBoundingBox();
+                                for (Text.Element element : line.getElements()) {
+                                    String elementText = element.getText();
+                                    Log.d(TAG, "onComplete: elemntText: " + elementText);
+                                    Log.d(TAG, "onComplete: \n");
+                                    Point[] elementCornerPoints = element.getCornerPoints();
+                                    Rect elementFrame = element.getBoundingBox();
+                                }
+
+                            }
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull @NotNull Exception e) {
+                        Log.d(TAG, "onFailure: lahhh napa si anjg");
+                    }
+                });
+            }
+        });
     }
 }
